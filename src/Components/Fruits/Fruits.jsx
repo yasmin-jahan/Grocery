e0166d549d769978585a39016a34b5a00dcd5ec3
yasmin-jahan <yasminjahan8603@gmail.com>
2026-08-03 14:56:24 +0530

    Add files via upload

--- src/Components/Fruits/Fruits.jsx
+++ src/Components/Fruits/Fruits.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-// import CategoryPage from '../CategoryPage/CategoryPage'
-// import BgFruits from '../../assets/fruits-banner.jpg'
+import CategoryPage from '../CategoryPage/CategoryPage'
+import BgFruits from '../../assets/fruits-banner.jpg'
 
 const Fruits = () => {
   return (
-    <div></div>
-  // <div>
-  //    <CategoryPage title="Fruits & Veggies" bgImage={BgFruits} categories={['Fruits', 'vegetables']}/>
-  // </div>
+  <div>
+     <CategoryPage title="Fruits & Veggies" bgImage={BgFruits} categories={['Fruits', 'vegetables']}/>
+  </div>
   )
 }
 
