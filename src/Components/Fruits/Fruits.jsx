fd12fcc09ebb3b3124992ffcb6188a3001f5ccc1
yasmin-jahan <yasminjahan8603@gmail.com>
2026-08-09 16:01:39 +0530

    Add files via upload

--- src/Components/Fruits/Fruits.jsx
+++ src/Components/Fruits/Fruits.jsx
@@ -5,9 +5,10 @@ import BgFruits from '../../assets/fruits-banner.jpg'
 const Fruits = () => {
   return (
   <div>
-     <CategoryPage title="Fruits & Veggies" bgImage={BgFruits} categories={['Fruits', 'vegetables']}/>
+     <CategoryPage title="Fruits & Veggies" bgImage={BgFruits} categories={['Fruits', 'Vegetables']}/>
   </div>
   )
 }
 
 export default Fruits
+
